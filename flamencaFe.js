4366dcc3d01e34e98c4b93e9aa730f322c92b948
VeveCambor <camborova.veronika@gmail.com>
2024-02-09 15:25:18 +0100

    add navigation for mobile version

--- flamencaFe.js
+++ flamencaFe.js
@@ -1,4 +1,19 @@
-function toggleMenu() {
-  var navbarLinks = document.querySelector(".nav-links");
-  navbarLinks.classList.toggle("active");
-}
+// function toggleMenu() {
+//   var navLinks = document.querySelector('.nav-links');
+//   navLinks.classList.toggle('active');
+// }
+
+// JavaScript
+// document.querySelector('.menu-icon').addEventListener('click', function() {
+//   var navLinks = document.querySelector('.nav-links');
+//   if (navLinks.style.display === 'none') {
+//     navLinks.style.display = 'block';
+//   } else {
+//     navLinks.style.display = 'none';
+//   }
+// });
+
+// JavaScript
+document.querySelector('.menu-icon').addEventListener('click', function() {
+  document.querySelector('.nav-links').classList.toggle('hide');
+});
