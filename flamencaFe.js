5edb9cd40219fc70e2fce3045c6aa906239bb38f
VeveCambor <camborova.veronika@gmail.com>
2024-02-10 16:38:15 +0100

    change bg-image and main section, EvenListener correction

--- flamencaFe.js
+++ flamencaFe.js
@@ -3,24 +3,6 @@ document.querySelector('.menu-icon').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('hide');
 });
 
-// document.querySelectorAll('.thumbnail').forEach(item => {
-//   item.addEventListener('click', event => {
-//     const mainImage = document.getElementById('mainImage');
-//     mainImage.src = item.getAttribute('data-img');
-//     mainImage.alt = item.alt;
-//   });
-// });
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   document.querySelectorAll('.gallery-item').forEach(item => {
-//     item.addEventListener('click', event => {
-//       const mainImage = document.getElementById('mainImage');
-//       mainImage.src = item.getAttribute('data-img');
-//       mainImage.alt = item.alt;
-//     });
-//   });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM fully loaded and parsed');
   document.querySelectorAll('.gallery-item').forEach(item => {
