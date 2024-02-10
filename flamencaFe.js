9590d63a4f4c33105a7dd589815a3c8f89d6e1cb
VeveCambor <camborova.veronika@gmail.com>
2024-02-10 15:12:53 +0100

    Band member cards and Gallery corrections

--- flamencaFe.js
+++ flamencaFe.js
@@ -2,3 +2,37 @@
 document.querySelector('.menu-icon').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('hide');
 });
+
+// document.querySelectorAll('.thumbnail').forEach(item => {
+//   item.addEventListener('click', event => {
+//     const mainImage = document.getElementById('mainImage');
+//     mainImage.src = item.getAttribute('data-img');
+//     mainImage.alt = item.alt;
+//   });
+// });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.querySelectorAll('.gallery-item').forEach(item => {
+//     item.addEventListener('click', event => {
+//       const mainImage = document.getElementById('mainImage');
+//       mainImage.src = item.getAttribute('data-img');
+//       mainImage.alt = item.alt;
+//     });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM fully loaded and parsed');
+  document.querySelectorAll('.gallery-item').forEach(item => {
+    console.log('Adding click event listener to gallery item');
+    item.addEventListener('click', event => {
+      console.log('Gallery item clicked');
+      const mainImage = document.getElementById('mainImage');
+      const newImageSrc = event.target.getAttribute('data-img');
+      console.log('New image src: ', newImageSrc);
+      mainImage.src = newImageSrc;
+      mainImage.alt = event.target.alt;
+    });
+  });
+});
+
