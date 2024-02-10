3966bb67ca5a0d34c511643ab7b0092459dc6240
VeveCambor <camborova.veronika@gmail.com>
2024-02-10 13:21:48 +0100

    add new pictures for gallery, rewrite section title to czech, css corrections

--- flamencaFe.js
+++ flamencaFe.js
@@ -1,18 +1,3 @@
-// function toggleMenu() {
-//   var navLinks = document.querySelector('.nav-links');
-//   navLinks.classList.toggle('active');
-// }
-
-// JavaScript
-// document.querySelector('.menu-icon').addEventListener('click', function() {
-//   var navLinks = document.querySelector('.nav-links');
-//   if (navLinks.style.display === 'none') {
-//     navLinks.style.display = 'block';
-//   } else {
-//     navLinks.style.display = 'none';
-//   }
-// });
-
 // JavaScript
 document.querySelector('.menu-icon').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('hide');
