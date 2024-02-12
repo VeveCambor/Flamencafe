4bb048b38330410599b746ee5ea26dd86c79b71d
VeveCambor <camborova.veronika@gmail.com>
2024-02-12 20:01:43 +0100

    add simple validation for contact form

--- flamencaFe.js
+++ flamencaFe.js
@@ -20,8 +20,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   AOS.init({
-  duration: 2000,
+    duration: 2000,
   });
 });
+
+const validateForm = () => {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  const errorElement = document.getElementById('error-message');
+  let errorMessage = '';
+
+  if (name === '' || name == null) {
+    errorMessage = 'Name is required';
+  }
+
+  if (email === '' || email == null) {
+    errorMessage = 'Email is required';
+  }
+
+  if (message === '' || message == null) {
+    errorMessage = 'Message is required';
+  }
+
+  if (errorMessage !== '') {
+    errorElement.innerText = errorMessage;
+    errorElement.style.display = 'block';
+    return false;
+  }
+}
+
+document.getElementById('contact-form').addEventListener('submit', validateForm);
 
 
